55567c79a6f625df6d65c4da191886b3547af094
Henrique Vinhal <henrique.vinhal@outlook.com>
2026-03-09 22:13:41 -0300

    Atividade 1 - Variáveis e tipos primitivos

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,18 @@
+let num_A = 10;
+let num_B = 2;
+
+
+let soma = num_A + num_B;
+console.log(`Resultado da soma entre ${num_A} e ${num_B} é: ${soma}`);
+
+
+let subtracao = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é: ${subtracao}`);
+
+
+let multiplicacao = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é: ${multiplicacao}`);
+
+
+let divisao = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é: ${divisao}`);
